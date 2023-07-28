5ef449670dd2dde00ae5ac04ea19dd9e99b7bf69
Oğuz Gül <oguz_gul216@hotmail.com>
2023-07-28 12:48:20 +0300

    Loader added.

--- src/pages/FeatureList.jsx
+++ src/pages/FeatureList.jsx
@@ -1,40 +1,51 @@
 import React, { useState, useEffect } from "react";
 import FeatureService from "../services/featureService";
-import {Container, Header, Divider } from "semantic-ui-react";
+import { Container, Header, Divider, Loader } from "semantic-ui-react";
 
 export default function FeatureList() {
   const [features, setFeature] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     let featureService = new FeatureService();
     featureService.getFeature().then((result) => setFeature(result.data.data));
+    setIsLoading(false);
   }, []);
   return (
-    <div >
-     {features.map((feature) => (
-         <Container text key={feature.featureID}>
-         <Header 
-           as='h1'
-           style={{
-             fontWeight: 'normal',
-             marginBottom: 0
-           }}
-         >{feature.header}</Header>
-         <Header
-           as='h2'
-           style={{
-             fontWeight: 'normal',
-           }}
-         >{feature.name}</Header>
-         <Header
-           as='h2'
-           style={{
-             fontWeight: 'normal',
-           }}
-         >{feature.title}</Header>
-       </Container>
-     ))}
-     <Divider></Divider>
+    <div>
+      {isLoading ? (
+        <Loader active inline />
+      ) : (
+        features.map((feature) => (
+          <Container text key={feature.featureID}>
+            <Header
+              as="h1"
+              style={{
+                fontWeight: "normal",
+                marginBottom: 0,
+              }}
+            >
+              {feature.header}
+            </Header>
+            <Header
+              as="h2"
+              style={{
+                fontWeight: "normal",
+              }}
+            >
+              {feature.name}
+            </Header>
+            <Header
+              as="h2"
+              style={{
+                fontWeight: "normal",
+              }}
+            >
+              {feature.title}
+            </Header>
+          </Container>
+        ))
+      )}
+      <Divider></Divider>
     </div>
   );
 }
